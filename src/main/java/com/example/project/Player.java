806f244577ede392d5472c14d3450319ae67b3bf
GavinY8 <gaviny8@nycstudents.net>
2025-02-28 13:24:57 +0000

    teacher

--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -1,5 +1,6 @@
 package com.example.project;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class Player{
@@ -16,21 +17,39 @@ public class Player{
     public ArrayList<Card> getAllCards(){return allCards;}
 
     public void addCard(Card c){
-        
+        hand.add(c);
     }
 
     public String playHand(ArrayList<Card> communityCards){      
         return "Nothing";
     }
 
+<<<<<<< HEAD
     public void sortAllCards(){} 
+=======
+    public void SortCards(){
+        for (int i = 1; i < hand.size(); i++) {
+            
+        }
+    } 
+>>>>>>> 03ee037 (merge stuff idk)
 
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freq = new ArrayList<>(13);
+        for (int i = 0; i < allCards.size(); i++) {
+            int idx = Utility.getRankValue(allCards.get(i).getRank())-2;
+            freq.set(idx, freq.get(i) + 1);
+        }
+        return freq; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> freq = new ArrayList<>(4);
+        ArrayList<String> suits = new ArrayList<>(Arrays.asList(Utility.getSuits()));
+        for (int i = 0; i < allCards.size(); i++) {
+            freq.set(suits.indexOf(allCards.get(i).getSuit()), freq.get(i) + 1);
+        }
+        return freq; 
     }
 
    
